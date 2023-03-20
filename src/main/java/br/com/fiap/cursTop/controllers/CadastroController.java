6371769bed74ede7604d23771db81fff8bd2ab89
pedrovidal <rm93567@fiap.com.br>
2023-03-20 16:55:49 -0300

    Delete, Put Cadastro

--- src/main/java/br/com/fiap/cursTop/controllers/CadastroController.java
+++ src/main/java/br/com/fiap/cursTop/controllers/CadastroController.java
@@ -6,16 +6,16 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.cursTop.models.Cadastro;
-import br.com.fiap.cursTop.models.ListaCadastro;
 
 
 @RestController
@@ -51,6 +51,40 @@ public class CadastroController {
         return ResponseEntity.ok(cadastroEncontrado.get());
 
     }
+
+    @DeleteMapping("/api/cadastro/{id}")
+    public ResponseEntity<Cadastro> destory(@PathVariable Long id){
+        log.info("Apagando usuario com id " + id);
+        var cadastroEncontrado = cadastros.stream().filter(d -> d.getId().equals(id)).findFirst();
+
+        if(cadastroEncontrado.isEmpty())
+             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        cadastros.remove(cadastroEncontrado.get());
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+    }
+
+    @PutMapping("/api/cadastro/{id}")
+    public ResponseEntity<Cadastro> update(@PathVariable Long id, @RequestBody Cadastro cadastro){
+        log.info("Atualizando usuario com id " + id);
+        var cadastroEncontrado = cadastros.stream().filter(d -> d.getId().equals(id)).findFirst();
+
+        if(cadastroEncontrado.isEmpty())
+             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        cadastros.remove(cadastroEncontrado.get());
+        cadastro.setId(id);
+        cadastros.add(cadastro);
+
+
+        return ResponseEntity.status(HttpStatus.OK).body(cadastro);
+
+    }
+
+
+
 }
 
 
---
+++ src/main/java/br/com/fiap/cursTop/controllers/MateriaController.java
@@ -0,0 +1,23 @@
+package br.com.fiap.cursTop.controllers;
+
+
+import java.math.BigDecimal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.cursTop.models.Materia;
+
+@RestController
+public class MateriaController {
+    
+    Logger log = LoggerFactory.getLogger(MateriaController.class);
+
+
+    
+
+}
