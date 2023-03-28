88ad61b0b5742853b90aefe5a37aa439656358ef
pedrovidal <rm93567@fiap.com.br>
2023-03-28 20:31:47 -0300

    Ajustes

---
+++ src/main/java/br/com/fiap/cursTop/controllers/PesquisaMateriaContoller.java
@@ -0,0 +1,33 @@
+package br.com.fiap.cursTop.controllers;
+
+import java.util.List;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.cursTop.models.Materia;
+import br.com.fiap.cursTop.repository.MateriaRepository;
+
+
+@RestController
+public class ListarMateriaContoller {
+
+    Logger log = LoggerFactory.getLogger(ListarMateriaContoller.class);
+
+    @Autowired
+    MateriaRepository repository;
+
+    @GetMapping
+    public List<Materia> index(){
+        return repository.findAll();
+    }
+
+    
+
+
+    
+}
